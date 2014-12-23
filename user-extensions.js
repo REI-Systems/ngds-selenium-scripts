973378f36a1a037453478d7e9cda103e415254df
sbalmuri-rei <swathi.balmuri@reisystems.com>
2014-12-23 16:26:50 -0500

    Globalvariablefile
    File where variables are declared globally

---
+++ user-extensions.js
@@ -0,0 +1,7 @@
+storedVars['username'] = "Registeredusername";
+storedVars['password'] = "Registeredpassword";
+storedVars['filepath'] = "C:\\swathi\\AZGS\\AR_WellLogs.csv";
+storedVars['Invalidfilepath'] = "C:\\swathi\\AZGS\\AR_WellLogs_INVALID.csv";
+storedVars['Incorrectfilepath'] = "C:\\swathi\\AZGS\\AR_WellLogs_.INCORRECT.csv";
+storedVars['Invalidfile'] = "C:\\swathi\\AZGS\\Test.doc";
+storedVars['Imageupload'] = "C:\\Users\\Public\\Pictures\\Sample Pictures\\Tulips.jpg";
